0add84c9122546e8c54e7226f98b7c7a2bb51f50
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-20 23:30:52 -0500

    handle text and audio

--- convert_ab.py
+++ convert_ab.py
@@ -3,22 +3,25 @@ from pathlib import Path
 from warnings import warn
 
 
-def convert(env: UnityPy.Environment, output_dir: Path):
-    texts = []
-    audio = []
-    gamedata = []
+def write_bytes(data: bytes, path: Path):
+    path.mkdir()
+    with open(path, "wb") as f:
+        f.write(data)
 
+
+def convert(env: UnityPy.Environment, output_dir: Path):
     for object in env.objects:
+        resource = object.read()
+        if hasattr(resource, "name"):
+            target_path = Path(output_dir, resource.name)
         match object.type.name:
             case "Sprite" | "Texture2D":
-                resource = object.read()
-                target_path = Path(output_dir, resource.name).with_suffix(".png")
-                resource.image.save(target_path)
+                resource.image.save(target_path.with_suffix(".png"))
             case "TextAsset":
-                texts.append(object.read())
+                write_bytes(bytes(resource.script), target_path)
             case "AudioClip":
-                audio.append(object.read())
+                write_bytes(bytes.join(resource.samples.items()), target_path)
             case "MonoBehaviour":
-                gamedata.append(object.read())
+                warn(f"Not implemented yet", RuntimeWarning)
             case _:
-                warn(f"Asset type '{object.type.name}' in the environment at '{env.path}' is not recognized.", RuntimeWarning)
+                warn(f"Asset type '{object.type.name}' at '{env.path}' is not recognized.", RuntimeWarning)
