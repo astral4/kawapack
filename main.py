13171b142369dc81e61f89a15a06eeae63303ab1
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-20 20:33:03 -0500

    load environments

---
+++ main.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from shutil import rmtree
+from warnings import warn
+import UnityPy
+
+def main(input_dir: str, output_dir: str, overwrite: bool = True):
+    input_path = Path(input_dir)
+    output_path = Path(output_dir)
+
+    if not (input_path.exists() and input_path.is_dir()):
+        warn("The input directory does not exist.", RuntimeWarning)
+        return
+    if not overwrite and output_path.exists() and output_path.is_dir():
+        warn("The output directory already exists, and the overwrite setting is set to False.", RuntimeWarning)
+        return
+
+    if output_path.is_dir():
+        rmtree(output_path)
+    output_path.mkdir()
+
+    for path in input_path.glob("**/*.ab"):
+        if path.is_file():
+            environment = UnityPy.load(path)
+            if len(environment.objects) > 0:
+                pass
+
+
+
+if __name__ == "__main__":
+    main("assets", "results")
