5bd6b5691f2c029a7aa3fb9a5571320bbacd0925
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-30 13:44:20 -0500

    rework target filepath handling

--- kawapack/convert_ab.py
+++ kawapack/convert_ab.py
@@ -1,27 +1,31 @@
 from UnityPy import Environment
-from UnityPy.classes import Sprite, Texture2D, TextAsset, AudioClip, MonoBehaviour, AssetBundle
+from UnityPy.classes import NamedObject, Sprite, Texture2D, TextAsset, AudioClip, MonoBehaviour
 from pathlib import Path
 import json
 import bson
 from Crypto.Cipher import AES
 
-def write_bytes(data: bytes, path: Path):
+
+def write_bytes(data: bytes, path: Path) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
     with open(path, "wb") as f:
         f.write(data)
 
-def write_object(data: object, path: Path):
+
+def write_object(data: object, path: Path) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
     with open(path.with_suffix(".json"), "w", encoding="utf8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def write_binary_object(data: bytes, path: Path):
+
+def write_binary_object(data: bytes, path: Path) -> None:
     try:
+        # BSON decoding fails if data is JSON-encoded BSON instead of plain BSON
         decoded = bson.decode(data)
         write_object(decoded, path)
     except:
-        # BSON decoding fails if data is JSON-encoded BSON instead of plain BSON
         write_object(json.loads(data), path)
+
 
 def decrypt_textasset(stream: bytes, start_index: int = 128) -> bytes:
     BITMASK = bytes.fromhex('554954704169383270484157776e7a7148524d4377506f6e4a4c49423357436c').decode()
@@ -45,52 +49,50 @@ def decrypt_textasset(stream: bytes, start_index: int = 128) -> bytes:
 
     return unpad(decrypted)
 
+
+def export(obj: NamedObject, target_path: Path) -> None:
+    match obj:
+        case Sprite() | Texture2D():
+            target_path.parent.mkdir(parents=True, exist_ok=True)
+            obj.image.save(target_path.with_suffix(".png"))
+        case TextAsset():
+            target_path_str = target_path.as_posix()
+            data = bytes(obj.script)
+            if "gamedata/story" in target_path_str:
+                # Story text is unencrypted, so the data can be directly written
+                write_bytes(data, target_path.with_suffix(".txt"))
+            elif "gamedata/levels/" in target_path_str:
+                data = decrypt_textasset(data, 0)
+                write_binary_object(data, target_path)
+            else:
+                try:
+                    # Decryption will fail if the data is not actually encrypted
+                    data = decrypt_textasset(data)
+                    write_binary_object(data, target_path)
+                except:
+                    try:
+                        write_object(json.loads(data), target_path)
+                    except:
+                        write_bytes(data, target_path)
+        case AudioClip():
+            for audio_name, audio_data in obj.samples.items():
+                write_bytes(audio_data, target_path.joinpath(audio_name).with_suffix(".wav"))
+        case MonoBehaviour():
+            tree = obj.read_typetree()
+            write_object(tree, target_path)
+
+
 def convert_from_env(env: Environment, output_dir: Path):
     for object in env.objects:
         if object.type.name in {"Sprite", "Texture2D", "TextAsset", "AudioClip", "MonoBehaviour"}:
             resource = object.read()
-
-            if isinstance(resource, (Sprite, Texture2D)):
-                target_path = output_dir.joinpath(env.path, resource.name)
-                target_path.parent.mkdir(parents=True, exist_ok=True)
-                resource.image.save(target_path.with_suffix(".png"))
-
-            elif isinstance(resource, TextAsset):
-                target_path = output_dir.joinpath(env.path, resource.name)
-                target_path_str = target_path.as_posix()
-                data = bytes(resource.script)
-
-                if "gamedata/story" in target_path_str:
-                    # Story text is unencrypted, so the data can be directly written
-                    write_bytes(data, target_path.with_suffix(".txt"))
-                elif "gamedata/levels/" in target_path_str:
-                    data = decrypt_textasset(data, 0)
-                    write_binary_object(data, target_path)
+            if isinstance(resource, NamedObject):
+                if resource.container:
+                    target_path = output_dir.joinpath(Path(resource.container).parent, resource.name)
                 else:
-                    try:
-                        # Decryption will fail if the data is not actually encrypted
-                        data = decrypt_textasset(data)
-                        write_binary_object(data, target_path)
-                    except:
-                        try:
-                            write_object(json.loads(data), target_path)
-                        except:
-                            write_bytes(data, target_path)
-            
-            elif isinstance(resource, AudioClip):
-                target_path = output_dir.joinpath(env.path, resource.name)
-                for audio_name, audio_data in resource.samples.items():
-                    write_bytes(audio_data, target_path.joinpath(audio_name).with_suffix(".wav"))
-            
-            elif isinstance(resource, MonoBehaviour):
-                if resource.name and not resource.name.isspace():
                     target_path = output_dir.joinpath(env.path, resource.name)
-                elif object.container:
-                    # TODO: check if this case is necessary
-                    target_path = output_dir.joinpath(object.container)
-                else:
-                    # TODO: explore this case further
-                    print(f"Did not serialize MonoBehaviour at {env.path}")
-                    continue
-                tree = resource.read_typetree()
-                write_object(tree, target_path)
+                export(resource, target_path)
+            else:
+                print(f"{object.type.name} object at {env.path} is not a NamedObject")
+        else:
+            print(f"{object.type.name} object at {env.path} was not processed")
